61572353295f14caba0a7087602ab1db8df5e8c3
CarrotPeeler <jared24mc@gmail.com>
2024-03-19 12:40:11 -0400

    fork

---
+++ dataset_paths.py
@@ -0,0 +1,106 @@
+DATASET_PATHS = [
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/cc3m',     
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/cc3m',
+        data_mode='dip',
+        key='cc3m'
+    ),
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/celeba-hq',   
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/celeba-hq',
+        data_mode='dip',
+        key='celeba-hq'
+    ),
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/cogview2-22K',   
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/cogview2-22K',
+        data_mode='dip',
+        key='cogview2-22k'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-ddim-50-15K',    
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-ddim-50-15K',
+        data_mode='dip',
+        key='if-ddim-50-15k'
+    ),
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-ddpm-50-15K',    
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-ddpm-50-15K',
+        data_mode='dip',
+        key='if-ddpm-50-15k'
+    ),
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-dpmsolver++-10-15K',    
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-dpmsolver++-10-15K',
+        data_mode='dip',
+        key='if-dpmsolver++-10-15K'
+    ),
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-dpmsolver++-25-15K',    
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/IF-CC95K/IF-dpmsolver++-25-15K',
+        data_mode='dip',
+        key='if-dpmsolver++-25-15K'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/Midjourneyv5-5K',    
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/Midjourneyv5-5K',
+        data_mode='dip',
+        key='midjourneyv5-5k'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/SDv15-CC30K/SDv15R-dpmsolver-25-15K',  
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/SDv15-CC30K/SDv15R-dpmsolver-25-15K',
+        data_mode='dip',
+        key='sdv15r-dpmsolver-25-15k'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/SDv21-CC15K/SDv2-dpmsolver-25-10K',   
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/SDv21-CC15K/SDv2-dpmsolver-25-10K',
+        data_mode='dip',
+        key='sdv2-dpmsolver-25-10K'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-r-afhqv2-512x512',   
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-r-afhqv2-512x512',
+        data_mode='dip',
+        key='stylegan3-r-afhqv2-512x512'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-afhqv2-512x512',   
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-afhqv2-512x512',
+        data_mode='dip',
+        key='stylegan3-t-afhqv2-512x512'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-ffhqu-1024x1024',   # Images from some video games
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-ffhqu-1024x1024',
+        data_mode='dip',
+        key='stylegan3-t-ffhqu-1024x1024'
+    ),
+
+
+    dict(
+        real_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-metfaces-1024x1024',   # Images from some video games
+        fake_path='/home/vislab-001/Jared/dip/CS545-Real-Fake-Image-Detection/sentry-dataset/ImageData/val/stylegan3-60K/stylegan3-t-metfaces-1024x1024',
+        data_mode='dip',
+        key='stylegan3-t-metfaces-1024x1024'
+    )
+]
